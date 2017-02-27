cd87d91e295db0f859a87c2f12645e0d0a78cae7
Maxb <maxb.personal@gmail.com>
2017-02-27 12:12:03 -0800

    Moving javascript to core.js; replacing getElementById with jquery; using jquery click handler instead of weird addtocalendar one.

---
+++ js/core.js
@@ -0,0 +1,12 @@
+$(function() {
+  
+  $('.addtocalendar').off('click');
+  $('.addtocalendar').on('click', function() {
+    console.log( "CLICKING HAPPENED" );
+    var calTitle = $("#inputTitle").val();
+    $("#calEventTitle").text(calTitle);
+    var calDesc = $("#inputDescription").val();
+    $("#calEventDescription").text(calDesc);
+  });
+
+});
