9068db1658e36771c32d4b3e51b5f43934413581
Digital Charlie <furman.jr@gmail.com>
2017-02-26 18:19:27 -0800

    hrm

--- js/core.js
+++ js/core.js
@@ -1,11 +1,10 @@
 $(function() {
-    $(".atcb-link").click(function(){
+    $(".addtocalendar").click(function(){
     	console.log( "clicked!" );
         var calTitle = document.getElementById("inputTitle").value;
         document.getElementById("calEventTitle").innerHTML = calTitle;
         var calDesc = document.getElementById("inputDescription").value;
         document.getElementById("calEventDescription").innerHTML = calDesc;
     });
-
 
 });
