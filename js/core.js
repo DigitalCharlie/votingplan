a847a81dce3ee035f461b955b2040e7a4953acc4
Digital Charlie <furman.jr@gmail.com>
2017-02-26 18:08:22 -0800

    ugh

--- js/core.js
+++ js/core.js
@@ -1,8 +1,11 @@
 $(function() {
-	if (window.addtocalendar)if(typeof window.addtocalendar.start == "function")return;
-	if (window.ifaddtocalendar == undefined) { window.ifaddtocalendar = 1;
-	    var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
-	    s.type = 'text/javascript';s.charset = 'UTF-8';s.async = true;
-	    s.src = ('https:' == window.location.protocol ? 'https' : 'http')+'://addtocalendar.com/atc/1.5/atc.min.js';
-	    var h = d[g]('body')[0];h.appendChild(s); }})();
+    $(".atcb-link").click(function(){
+    	console.log( "clicked!" );
+        var calTitle = document.getElementById("inputTitle").value;
+        document.getElementById("calEventTitle").innerHTML = calTitle;
+        var calDesc = document.getElementById("inputDescription").value;
+        document.getElementById("calEventDescription").innerHTML = calDesc;
+    });
 
+
+});
