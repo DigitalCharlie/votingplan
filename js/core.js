fea2335fe4734524c26b4f2024b4766a055804dc
Aki Rose Braun <hi@akiro.se>
2017-10-21 02:27:06 -0700

    Add calendar makes form submit now?

--- js/core.js
+++ js/core.js
@@ -13,15 +13,6 @@ $(function() {
 
 });
 
-$(function() {
-  
-  $('.addtocalendar').on('click', function() {
-  	$("#new_answer").submit();
-  });
-
-});
-
-
 $(".want-an-email").change(function() {
     if ( $(this).is(':checked') ) {
     	$("#can-form-area-dont-forget-the-midterms").css('display', 'block');
@@ -61,3 +52,42 @@ function calendarClick () {
 	  });
 }
 
+window.addEventListener('load', function () {
+  var anForm = document.getElementById('new_answer');
+
+  document.getElementById('form-email').setAttribute('required', true);
+  document.getElementById('form-zip_code').setAttribute('required', true);
+
+  anForm.removeAttribute('novalidate');
+  anForm.elements.commit.classList.add('hidden');
+
+  document.getElementById('calendar-button').addEventListener('click', function () {
+    if (anForm.checkValidity()) {
+      anForm.submit();
+    }
+  });
+
+  (function () {
+    if (window.addtocalendar && typeof window.addtocalendar.start === 'function') return;
+    if (window.ifaddtocalendar === undefined) {
+      window.ifaddtocalendar = 1;
+      var
+        d = document,
+        s = d.createElement('script'),
+        g = 'getElementsByTagName';
+
+      s.type = 'text/javascript';
+      s.charset = 'UTF-8';
+      s.async = true;
+      s.src = ('https:' === window.location.protocol ? 'https' : 'http') + '://addtocalendar.com/atc/1.5/atc.min.js';
+      var h = d[g]('body')[0];
+      h.appendChild(s);
+    }
+  }());
+});
+
+
+$(document).on('can_embed_submitted', function () {
+  $('#dont-forget-the-midterms').css('display', 'none');
+  $('#can-form-thanks').removeClass('hidden').fadeIn();
+});
