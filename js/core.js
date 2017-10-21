ecdeedfdd9acd87a46513ee227faeea8fb5c4ec3
Digital Charlie <furman.jr@gmail.com>
2017-10-21 13:00:18 -0700

    Revert "Revert "Integrating Action Network form submit and addtocalendar show/hide""

--- js/core.js
+++ js/core.js
@@ -1,63 +1,53 @@
-$(function() {
-  
-  $('.addtocalendar').off('click');
-  $('.addtocalendar').on('click', function() {
-    console.log( "CLICKING HAPPENED" );
-    var calTitle = $("#inputTitle").val();
-    $("#calEventTitle").text(calTitle);
-    var calDesc = $("#inputDescription").val();
-    $("#calEventDescription").text(calDesc);
 
-    addtocalendar.load();
-  });
+function loadCalendar() {
+  var calTitle = $("#inputTitle").val();
+  $("#calEventTitle").text(calTitle);
+  var calDesc = $("#inputDescription").val();
+  $("#calEventDescription").text(calDesc);
 
-});
-
-$(function() {
-  
-  $('.addtocalendar').on('click', function() {
-  	$("#new_answer").submit();
-  });
-
-});
-
-
-$(".want-an-email").change(function() {
-    if ( $(this).is(':checked') ) {
-    	$("#can-form-area-dont-forget-the-midterms").css('display', 'block');
-    	setTimeout(function() {
-        	$("#can-form-area-dont-forget-the-midterms").css('opacity', 1);
-    	}, 30);
-    } else {
-    	$("#can-form-area-dont-forget-the-midterms").css('display', 'none');
-    	setTimeout(function() {
-        	$("#can-form-area-dont-forget-the-midterms").css('opacity', 0);
-        }, 30);
-    }
-});
-
-function calendarClick () {
-
-	  var showModal = function(selector) {
-	    $(selector).css('display', 'block');
-	    setTimeout(function() {
-	      $(selector).css('opacity', 1);
-	    }, 50);
-	  };
-
-	  var hideModal = function(selector) {
-	    $(selector).css('display', 'none');
-	    setTimeout(function() {
-	      $(selector).css('opacity', 0);
-	    }, 50);
-	  };
-
-	  setTimeout(function(){
-	    showModal('#shareModal');
-	  }, 1000);
-
-	  $("#shareX").click(function() {
-	    hideModal('#shareModal');
-	  });
+  addtocalendar.load();
 }
 
+$(function() {
+  
+  $('.addtocalendar a.atcb-link').on('click', function(event) {
+    console.log('clicked?');
+  });
+
+  $('.addtocalendar').off('mousedown');
+  $('.addtocalendar').on('mousedown', function(event) {
+    event.preventDefault();
+    // event.stopImmediatePropagation();
+
+    console.log( "CLICKING HAPPENED" );
+
+    if($(".want-an-email").is(':checked')) {
+      $('#can-form-area-dont-forget-the-midterms input[type="submit"]').trigger('click');
+    } else {
+      loadCalendar();
+      $('.addtocalendar .atcb-link').focus();
+    }
+  });
+
+  $(document).on('can_embed_submitted', function() {
+    // $('#dont-forget-the-midterms').css('display','none');
+    // $('#can-form-thanks').removeClass('hidden').fadeIn();
+    $('form.email-reminder').hide();
+    loadCalendar();
+    $('.addtocalendar .atcb-link').focus();
+  });
+
+  $(".want-an-email").change(function() {
+      if ( $(this).is(':checked') ) {
+        $("#can-form-area-dont-forget-the-midterms").css('display', 'block');
+        setTimeout(function() {
+            $("#can-form-area-dont-forget-the-midterms").css('opacity', 1);
+        }, 30);
+      } else {
+        $("#can-form-area-dont-forget-the-midterms").css('display', 'none');
+        setTimeout(function() {
+            $("#can-form-area-dont-forget-the-midterms").css('opacity', 0);
+          }, 30);
+      }
+  });
+});
