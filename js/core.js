1c6f42f7382d369748b0e4adde69629e4b293db4
Digital Charlie <furman.jr@gmail.com>
2017-10-20 22:06:15 -0700

    big updates to features

--- js/core.js
+++ js/core.js
@@ -12,3 +12,52 @@ $(function() {
   });
 
 });
+
+/*$(function() {
+  
+  $('.addtocalendar').on('click', function() {
+  	$("#new_answer").submit();
+  });
+
+});
+*/
+
+$(".want-an-email").change(function() {
+    if ( $(this).is(':checked') ) {
+    	$("#can-form-area-dont-forget-the-midterms").css('display', 'block');
+    	setTimeout(function() {
+        	$("#can-form-area-dont-forget-the-midterms").css('opacity', 1);
+    	}, 30);
+    } else {
+    	$("#can-form-area-dont-forget-the-midterms").css('display', 'none');
+    	setTimeout(function() {
+        	$("#can-form-area-dont-forget-the-midterms").css('opacity', 0);
+        }, 30);
+    }
+});
+
+function calendarClick () {
+
+	  var showModal = function(selector) {
+	    $(selector).css('display', 'block');
+	    setTimeout(function() {
+	      $(selector).css('opacity', 1);
+	    }, 50);
+	  };
+
+	  var hideModal = function(selector) {
+	    $(selector).css('display', 'none');
+	    setTimeout(function() {
+	      $(selector).css('opacity', 0);
+	    }, 50);
+	  };
+
+	  setTimeout(function(){
+	    showModal('#shareModal');
+	  }, 1000);
+
+	  $("#shareX").click(function() {
+	    hideModal('#shareModal');
+	  });
+}
+
