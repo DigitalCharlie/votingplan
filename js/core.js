060ac309517d25818f309a926df1a42931ef659a
Digital Charlie <furman.jr@gmail.com>
2017-10-21 13:04:20 -0700

    added modal back in

--- js/core.js
+++ js/core.js
@@ -51,3 +51,27 @@ $(function() {
       }
   });
 });
+
+function calendarClick () {
+    var showModal = function(selector) {
+      $(selector).css('display', 'block');
+      setTimeout(function() {
+        $(selector).css('opacity', 1);
+      }, 50);
+    };
+
+    var hideModal = function(selector) {
+      $(selector).css('display', 'none');
+      setTimeout(function() {
+        $(selector).css('opacity', 0);
+      }, 50);
+    };
+
+    setTimeout(function(){
+      showModal('#shareModal');
+    }, 1000);
+
+    $("#shareX").click(function() {
+      hideModal('#shareModal');
+    });
+}
